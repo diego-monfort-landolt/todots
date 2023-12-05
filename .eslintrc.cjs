8da41bb032a5d86b400a3df7f0e7e874161c3f1a
Diego-monfort-landot <diegomonfort1989@gmail.com>
2023-12-05 19:47:23 +0100

    added new type

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -29,6 +29,7 @@ module.exports = {
         "react"
     ],
     "rules": {
+        "react/prop-types": "off",
         "react/react-in-jsx-scope": "off",
     }
 }
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,4 +1,15 @@
-export const Todos: React.FC = ({ todos }) => {
+interface Todo {
+  id: string
+  title: string
+  complete: boolean
+}
+
+type ListOfTodos = Todo[]
+interface Props {
+  todos: ListOfTodos
+}
+
+export const Todos: React.FC<Props> = ({ todos }) => {
   return (
     <ul>
       {todos.map(todo => (
